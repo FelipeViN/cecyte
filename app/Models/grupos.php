8169a3c6aa883719eb8b1ba67aac8ffdc220f06b
AdielDC <105898814+AdielDC@users.noreply.github.com>
2024-12-14 11:43:13 -0600

    Agregacion  de relaciones con las tablas para poder trabajar

--- app/Models/grupos.php
+++ app/Models/grupos.php
@@ -9,4 +9,21 @@ class grupos extends Model
 {
     use HasFactory;
     protected $fillable = ['nombre', 'semestre', 'cicloEscolar'];
+     // Relación con estudiantes
+   public function estudiantes()
+   {
+       return $this->belongsToMany(Estudiantes::class, 'alumno_grupo', 'grupo_id', 'estudiante_id');
+   }
+
+
+    // Relación muchos a muchos con Materias
+    public function materias()
+    {
+        return $this->belongsToMany(Materias::class, 'grupo_materia');  // Tabla intermedia 'grupo_materia'
+    }
+
+    public function calificaciones()
+{
+    return $this->hasMany(CalificacionEducativa::class);
+}
 }
